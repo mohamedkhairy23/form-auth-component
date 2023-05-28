2ebe25933e585da847105f4f5b470228f1ff225d
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-05-28 12:16:11 +0300

    initial-commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 8888;
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Server is ready"));
+
+app.listen(port, () => console.log(`server started on port ${port}`));
